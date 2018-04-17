513af5f8e13867e9da0fd1fcca91bcd7491249de
Nathan Chancellor <natechancellor@gmail.com>
2018-04-16 21:11:38 -0700

    oneplus5: Build the kernels with Clang
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -77,7 +77,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8998
 TARGET_KERNEL_CONFIG := lineage_oneplus5_defconfig
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_CLANG_COMPILE := true
 
 # Sepolicy
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
