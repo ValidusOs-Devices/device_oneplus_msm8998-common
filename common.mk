05621f5722d36a9608b04de62448592f32710a61
Carlos Solano <csolano@gmail.com>
2019-01-08 08:11:18 +0100

    msm8998: remove lineage security prop

--- common.mk
+++ common.mk
@@ -453,10 +453,6 @@ PRODUCT_PACKAGES += \
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_prop.mk
 
-# Vendor security patch level
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.lineage.build.vendor_security_patch=2018-12-01
-
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
