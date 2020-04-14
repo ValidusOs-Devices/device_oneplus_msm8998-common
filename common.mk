5660f5012da9be89f970e999921e9b8bf58530f9
martinusbe <martinusbe@gmail.com>
2020-04-14 02:28:20 +0200

    msm8998-common: remove pocketmode

--- common.mk
+++ common.mk
@@ -232,8 +232,7 @@ PRODUCT_PACKAGES += \
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service \
-    fingerprintd \
-    OneplusPocketMode
+    fingerprintd
 
 # For android_filesystem_config.h
 PRODUCT_PACKAGES += \
